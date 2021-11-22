f2913574ebf598fa94b771df7697763936867f61
Ricky Nelson <rickyn@socketwiz.com>
2021-11-22 07:21:38 -0500

    nvim path

--- default-difftool.sh
+++ default-difftool.sh
@@ -4,14 +4,14 @@
 #
 
 # try neovim first
-if [ -f /usr/local/bin/nvim ]
+if [ -f /usr/bin/nvim ]
 then
-  binary="/usr/local/bin/nvim"
+  binary="/usr/bin/nvim"
   VIMPATH="$binary -d"
-elif [[ -f /usr/local/bin/mvim ]]
+elif [[ -f /usr/bin/mvim ]]
 then
   # fall back to mvim
-  VIMPATH='/usr/local/bin/mvim -d -f'
+  VIMPATH='/usr/bin/mvim -d -f'
 else
   # fall back to original vim
   VIMPATH='vimdiff'
