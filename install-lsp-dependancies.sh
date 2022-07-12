463e69895e8faca1f4ac2229f58e7f3926d3c84c
Ricky Nelson <rickyn@getpicnic.com>
2022-07-12 18:42:15 -0400

    minor fixes and tweaks

--- install-lsp-dependancies.sh
+++ install-lsp-dependancies.sh
@@ -1,21 +1,21 @@
 #! /usr/bin/env bash
 
 # Angular
-sudo npm install -g @angular/language-server
-sudo npm install -g @angular/language-service
+npm install -g @angular/language-server
+npm install -g @angular/language-service
 #
 # Bash
-sudo npm install -g bash-language-server
+npm install -g bash-language-server
 #
 # Javascript / TypeScript
-sudo npm install -g typescript
-sudo npm i -g typescript-language-server
+npm install -g typescript
+npm install -g typescript-language-server
 #
 # HTML
-sudo npm install -g vscode-html-languageserver-bin
+npm install -g vscode-html-languageserver-bin
 #
 # JSON
-sudo npm install -g vscode-json-languageserver
+npm install -g vscode-json-languageserver
 #
 # Python
 pip3 install python-language-server\[all\]
