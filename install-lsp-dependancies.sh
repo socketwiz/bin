4e6a0aa5e2f122c88478a639860d68de0dcdf96c
Ricky Nelson <rickyn@socketwiz.com>
2025-02-05 21:19:14 -0500

    current script updates/additions

--- install-lsp-dependancies.sh
+++ install-lsp-dependancies.sh
@@ -9,6 +9,7 @@ npm install -g prettier-eslint-cli
 npm install -g prettier
 npm install -g react-devtools
 npm install -g vscode-langservers-extracted
+npm install -g typescript typescript-language-server
 #
 # HTML
 npm install -g vscode-html-languageserver-bin
